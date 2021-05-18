001dc099ec483fa0850d69d6597674c118995140
JOker323232 <84375904+JOker323232@users.noreply.github.com>
2021-05-18 11:29:40 +0300

    Mod botu

--- ayarlar.js
+++ ayarlar.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "token": "BOTUNUZUN TOKENİ",
-  "sahip": "SAHİP ID",
-  "prefix": "BOTUNUZUN PREFİXİ"
+  "token": "ODQ0MTIyMzI4OTAzNTgxNzU2.YKN0Lg.BrMzih13dbP_NIvwZi9x2KPFTSA",
+  "sahip": "828647521864908841",
+  "prefix": "x!"
 };
